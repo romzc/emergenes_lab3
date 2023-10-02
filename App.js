5619c4750335e147c4a042a089e6b2459eef91c4
John Dow <ajacoboc@unsa.edu.pe>
2023-10-01 22:51:58 -0500

    LOCAL BRANCH INIT

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+const data = [
+  { key: "1", title: "Tarea 1" },
+  { key: "2", title: "Tarea 2" },
+];
+
+const renderItem = ({ item }) => {
+  return <Text>{item.title}</Text>;
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* Título de la aplicación */}
+      <Text style={styles.title}>TodoApp</Text>
+      {/* Contenedor del card */}
+      <View style={styles.card}>
+        {/* Lista de tareas */}
+        <FlatList data={data} renderItem={renderItem} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  card: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    padding: 20,
+    width: "80%",
   },
 });
